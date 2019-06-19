09a04b2907cda07fc0e5938b34d1e7056874fa4c
mchoir24 <miftahulkhoir9@gmail.com>
2019-06-19 20:51:20 +0700

    menimpan ke database

--- app/src/main/java/com/example/projectppb/Form.java
+++ app/src/main/java/com/example/projectppb/Form.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,23 +17,38 @@ public class Form extends AppCompatActivity {
     protected Cursor cursor;
     TextView teksatas,teksKat,teksJudul,teksDue;
     private Spinner sp_kategori;
-    private String[] list = {"-Kategori-", "Penting Mendesak", "Penting Tidak Mendesak", "Tidak Penting Mendesak", "Tidak Penting Tidak Mendesak"};
+   // private String[] list = {"-Kategori-", "Penting Mendesak", "Penting Tidak Mendesak", "Tidak Penting Mendesak", "Tidak Penting Tidak Mendesak"};
+
+    String[] daftar;
+    DataHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        teksatas = (TextView) findViewById(R.id.txt_tambah_list);
-        teksJudul= (TextView) findViewById(R.id.txt_judul);
-        teksDue = (TextView) findViewById(R.id.txt_due);
-        teksKat = (TextView) findViewById(R.id.txt_spin);
+        dbHelper = new DataHelper(this);
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM kategori",null);
+        daftar = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int cc=0; cc < cursor.getCount(); cc++){
+            cursor.moveToPosition(cc);
+            daftar[cc] = cursor.getString(1);
+        }
+
+        teksatas = findViewById(R.id.txt_tambah_list);
+        teksJudul= findViewById(R.id.txt_judul);
+        teksDue = findViewById(R.id.txt_due);
+        teksKat = findViewById(R.id.txt_spin);
+
 
         sp_kategori = (Spinner) findViewById(R.id.spinner);
         sp_kategori.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                teksKat.setText(list[position]);
+                //teksKat.setText(list[position]);
             }
 
             @Override
@@ -41,19 +57,23 @@ public class Form extends AppCompatActivity {
             }
         });
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar);
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         sp_kategori.setAdapter(adapter);
 
     }
 
-    public void buttonPesan(View view) {
-        //SQLiteDatabase db = dbHelper.getWritableDatabase();
-        //db.execSQL("insert into note(judul, kategori, due) values('" +
-                //teksJudul.getText().toString() + "','" +
-                //teksDue.getText().toString() + "','" +
-                //sp_kategori.getSelectedItemId() + "')");
-        //Toast.makeText(getApplicationContext(), "Data Tersimpan", Toast.LENGTH_SHORT).show();
+    public void tambah(View view) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        cursor = db.rawQuery("SELECT * FROM kegiatan",null);
+        cursor.moveToLast();
+        Log.d("tambah: ", String.valueOf(sp_kategori.getSelectedItemPosition())  );
+        db.execSQL("insert into `kegiatan`(`id_kegiatan`,`nama`, `tgl`, `id_kategori`) values('" +
+                cursor.getInt(0)+1+ "','" +
+                teksJudul.getText().toString() + "','" +
+                teksDue.getText().toString() + "','" +
+                sp_kategori.getSelectedItemPosition()+1 + "')");
+        Toast.makeText(getApplicationContext(), "Data Tersimpan", Toast.LENGTH_SHORT).show();
         finish();
     }
 }
